3bb654ea43ec7a7abc9f7f3649795a55797ec16e
Phelipe de Sterlich <p.desterlich@gmail.com>
2016-01-17 14:17:52 +0100

    added Person entity
    added basic Person entity (with firstName and lastName properties) and
    relative NSManagedObject subclass

---
+++ CoreDataTest/CoreDataTest/Person+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  Person+CoreDataProperties.swift
+//  CoreDataTest
+//
+//  Created by Phelipe de Sterlich on 17.01.16.
+//  Copyright © 2016 Phelipe de Sterlich. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Person {
+
+    @NSManaged var firstName: String?
+    @NSManaged var lastName: String?
+
+}
---
+++ CoreDataTest/CoreDataTest/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  CoreDataTest
+//
+//  Created by Phelipe de Sterlich on 17.01.16.
+//  Copyright © 2016 Phelipe de Sterlich. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Person: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
